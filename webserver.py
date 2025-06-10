e85f934e827bcf305db442a88eecbba56eb519a5
perry <shubdeshpa@gmail.com>
2025-06-10 14:17:52 -0500

    idk pt 2

--- webserver.py
+++ webserver.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from threading import Thread
-
+import os
 
 app = Flask('')
 @app.route('/')
@@ -10,7 +10,7 @@ def home():
 def run():
     port = int(os.environ.get("PORT", 8080))  # Use PORT env var if set, else 8080
     app.run(host='0.0.0.0', port=port)
-    
+
 def keep_alive():
     t = Thread(target=run)
     t.start()
